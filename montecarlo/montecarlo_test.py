e247eb75a391a5d26f3d55fbcaacacb010830610
Alex Yang <alexyoung0204@gmail.com>
2024-07-11 14:36:31 -0400

    finalizing die() class unittest

--- montecarlo/montecarlo_test.py
+++ montecarlo/montecarlo_test.py
@@ -33,6 +33,36 @@ class MonteCarloTest(unittest.TestCase):
         #testing
         self.assertTrue(die.printdie().equals(expected_die))
 
+    def test_die_rolldie(self):
+        """
+        A test function to test the roll of a Die object.
+        """
+        #initializing the die
+        array = np.array([1,2,3,4,5,6])
+        die = mt.Die(array)
+        #rolling the die
+        outcome = len(die.rolldie(3))
+        #expecting
+        expected = 3
+        #testing
+        self.assertTrue(np.array_equal(outcome, expected))
+
+    def test_printdie(self):
+        """
+        A test function to test the return of die's data frame
+        """
+        #initializing the die
+        array = np.array([1,2,3,4,5,6])
+        die = mt.Die(array)
+        #expecting
+        expected_df = pd.DataFrame({
+            'faces': [1, 2, 3, 4, 5, 6],
+            'weight': [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+        })
+        # Check if the returned DataFrame from printdie() matches the expected DataFrame
+        self.assertTrue(die.printdie().equals(expected_df))
+
+    
 
 if __name__ == '__main__':
     unittest.main()
