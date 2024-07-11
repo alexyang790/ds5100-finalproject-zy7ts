93289fed13d698131eab90bbeba0034234676e94
Alex Yang <alexyoung0204@gmail.com>
2024-07-10 21:29:05 -0400

    updating change weight method in Die() class

--- montecarlo/montecarlo.py
+++ montecarlo/montecarlo.py
@@ -33,17 +33,22 @@ class Die():
             {'faces': self.faces,
             'weight': self.__weights}
         )
-        
-        def changeweight(self, face, new_weight):
-            #checking if face is valid i.e. if it is in the die array
-            if face not in self.__die['faces'].values:
-                raise IndexError('provided face not in die')
-            
-            #checking if new_weight is valid
-            
-            
 
-      
+    def changeweight(self, face, new_weight):
+        #checking if face is valid i.e. if it is in the die array
+        if face not in self.__die['faces'].values:
+            raise IndexError('provided face not in die')
+        
+        #checking if new_weight is valid
+        if not isinstance(new_weight, (int, float)):
+            raise TypeError('provided weight is not numeric or not castable as numeric')
+
+        #changing the weight
+        self.__die.loc[self.__die['faces'] == face, 'weight'] = new_weight
+
+    def printdie(self):
+        return self.__die
+
 
 class Game():
     def __init__(self):
@@ -52,35 +57,3 @@ class Game():
 class Analyzer():
     def __init__(self):
         pass
-
-
-import pandas as pd
-class BookLover():
-    def __init__(self, name, email, fav_genere):
-        self.name = name
-        self.email = email
-        self.fav_genere = fav_genere
-        self.num_books = 0
-        self.book_list = pd.DataFrame({'book_name':[], 'book_rating':[]})
-    
-    def add_book(self, book_name, rating):
-        if not book_name in self.book_list['book_name'].values: #checking if book already exists
-            #frame new book & rating and concat to book list
-            new_book = pd.DataFrame({
-                'book_name': [book_name],
-                'book_rating': [rating]
-            })
-            self.book_list = pd.concat([self.book_list, new_book], ignore_index=True)
-            self.num_books += 1
-
-    def has_read(self, book_name):
-        if book_name in self.book_list['book_name'].values:
-            return True
-        else:
-            return False
-    
-    def num_books_read(self):
-        return self.num_books
-
-    def fav_books(self):
-        return self.book_list[self.book_list['book_rating'] > 3]
